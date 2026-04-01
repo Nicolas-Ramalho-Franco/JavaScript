70a9fed6d0185baae9b9f80c9af6d444319d2222
Nicolas <nicolasramalhof@gmail.com>
2026-04-01 20:55:06 -0300

    aprendendo

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert("Ola mundo");//um pop-up na tela
+// prompt("Qaul o seu nome")//tem como voce digitar algo
+// confirm("Voce deseja sair")//isso apresenta na tela algo para confirmar
+// console.log("oioioioiooioioioiooioioi");//faz aparecer algo na tela
+
+// let valor pode variar
+// const vai ser constante
+// typeof no inicio da variavel ele tras o tipo da variavel
+// let nome = "Nicolas";
+// const pi = 3.1415; 
+// alert(nome);
+
+let nome =prompt("Qual e o seu nome")
+alert(`Seu nome e ${nome}`)     //isso pega o q foi escrito no pop-up e guarda
+// a crese ela tem q ser colocada e o ${} e para mostrar alguma variavel dentro da mensagem
